a1fcd3c172aa362ed3b41485d97b93413588f72d
CyberFlame <cyberflameu@gmail.com>
2020-10-19 12:11:25 +1300

    add new action

--- main.ts
+++ main.ts
@@ -44,5 +44,21 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showIcon(IconNames.Scissors)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
 })
