ebe3bb843a51d179f8832f18d3802c057f2d1ea2
CyberFlame <cyberflameu@gmail.com>
2020-10-19 12:16:20 +1300

    dumb shit

--- main.ts
+++ main.ts
@@ -43,22 +43,51 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
     basic.showIcon(IconNames.Heart)
 })
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . . . .
+        . # . # .
+        . # . # .
+        `)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . # . . .
+        # . . # #
+        # . . . .
+        # . . # #
+        . # . . .
+        `)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Scissors)
 })
 input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . # .
+        # # . . #
+        . . . . #
+        # # . . #
+        . . . # .
+        `)
+})
+input.onGesture(Gesture.LogoDown, function () {
     basic.showLeds(`
         . # . # .
         . # . # .
         . . . . .
         # . . . #
         . # # # .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . . . . .
-        . . # . .
         `)
 })
